f7359fa5f51faa6e2568899afed4b7caeac3b2eb
Okan Demirbilek <48480726+okademirbilek@users.noreply.github.com>
2023-07-20 17:54:44 +0300

    router path changed

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import { BrowserRouter as Router } from "react-router-dom";
 import { ContextProvider } from "./Context";
 import App from "./App";
 import "./index.css";
@@ -8,9 +7,7 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
   <ContextProvider>
-    {/* <Router> */}
     <App />
-    {/* </Router> */}
   </ContextProvider>
   // </React.StrictMode>
 );
